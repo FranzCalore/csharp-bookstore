67753d7f882fa9000fb5652af062f01a3fdde9ca
FranzCalore <franzkaiser1993@gmail.com>
2022-11-22 13:27:20 +0100

    Completato l'assegnamento di base

--- CSharp-Bookstore/Program.cs
+++ CSharp-Bookstore/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string titolo = "Lo Squalificato";
+string autore = "Osamu Dazai";
+string isbn = "8804755784";
+int numeroPagine = 120;
+int pesoLibro = 200;
+double larghezza = 14.3;
+double altezza = 21.2;
+double profondità = 1.3;
+double valutazione = 4.3;
+int numeroRecensioni = 21;
+string kindle = "Sì";
+string flessibile = "Sì";
+
+Console.WriteLine($"------------ {titolo} di {autore} ----------------");
+Console.WriteLine();
+Console.WriteLine($"Informazioni generiche: \nISBN: {isbn} \nNumero delle pagine: {numeroPagine}\nPeso del libro: {pesoLibro}\nDimensioni del libro: {larghezza} X {profondità} X {altezza}");
+Console.WriteLine();
+Console.WriteLine($"Informazioni di Amazon: \nNumero di recensioni: {numeroRecensioni}\nValutazione media: {valutazione}/5\nKindle disponibile: {kindle}\nCopertina flessibile disponibile: {flessibile}");
+Console.WriteLine();
+Console.WriteLine("-------------------------------------------------------------");
+
